4fb79c850619296c984f94a70a10f0cd008a8113
rururing <76811232+rururing@users.noreply.github.com>
2024-07-15 08:38:39 +0900

    .

---
+++ Assets_dst/Script/Onclick_Nextscreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Onclick_Nextscreen : MonoBehaviour
+{  
+    public GameObject nextscreen;
+    public void Next_Scene(string a)
+    {
+        StartCoroutine(start_effect(a));
+    }
+
+    IEnumerator start_effect(string a)
+    {
+        nextscreen.SetActive(true);
+        float alpha = 0.0f;
+        while (alpha < 1)
+        {
+            alpha += 0.02f;
+            nextscreen.GetComponent<Image>().color = new Color(1, 1, 1, alpha);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+        SceneManager.LoadScene(a);
+    }
+}
+
+
---
+++ Assets_dst/Script/Story_Text.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Story_Text : MonoBehaviour
+{
+    private bool toggle = false;
+
+    public GameObject Script_Screen;
+    public void Panel_Set()
+    {
+        toggle = !toggle;
+        Script_Screen.SetActive(toggle);
+    }
+}
